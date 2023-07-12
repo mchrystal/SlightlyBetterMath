1689f94c1238d8f69be48a6540ff71dc80300f0f
mchrystal <137828954+mchrystal@users.noreply.github.com>
2023-07-12 08:38:17 -0400

    Offline Local Changes - Logic and Math Fixes
    In average()
    -Line 7: Added Parenthesis around "i1 + i2 + i3" //This fixes a math error
    In main()
    -Between Line 12/13 & 20/21 (Now 13 & +25): Added Greeting and Closing
    -Line 14-16 (Now 15, 17, 19): Added "Enter the " to "[nth] Number: " for extra user-friendliness.
    -Line 14-16 (Now 14-20): Separated cout and cin //Personal Preference, in case of increasing program complexity.
    -Line (Now 20): Changed "cin >> n2" to "cin >> n3" //n2 is the wrong variable

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,20 +4,25 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
+	cout << "Welcome to the The Three Number Averaging Program." << endl;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Enter the First number: "; 
+	cin >> n1;
+	cout << "Enter the Second number: "; 
+	cin >> n2;
+	cout << "Enter the Third number: "; 
+	cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(1) << fixed << "The average is " << a << "." << endl;
+	cout << "Thank you for using The Three Number Averaging Program." << endl;
 
 	return 0;
 }
